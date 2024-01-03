72d35b66eb2c2be8dfae6c49aebcc7a424008d43
Tommy Leung <tommyleungliverpool@gmail.com>
2024-01-03 14:31:20 -0800

    feat: basic design of the meme generator

---
+++ meme/src/components/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header(){
+    return(
+        <div>
+            <header className="header">
+                <img className="header--image" src="../images/Troll Face.png"></img>
+                <h2 className="header--title">Meme Generator</h2>
+            </header>
+        </div>
+    )
+}
