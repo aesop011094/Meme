767c176f8d0de149c19b201b4e1f4876306f5b8e
Tommy Leung <tommyleungliverpool@gmail.com>
2024-01-08 21:08:03 -0800

    feat: get random meme from api

--- meme/src/components/Meme.js
+++ meme/src/components/Meme.js
@@ -1,11 +1,42 @@
+import React, {useState , useEffect} from "react"
 export default function Meme(){
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    });
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    },[])
+    
+    function getMemeImage(){
+        if (allMemes.length === 0) {
+            console.error("No meme templates available");
+            return;
+        }
+        
+        const randomIndex = Math.floor(Math.random() * allMemes.length)
+        const randomMeme = allMemes[randomIndex]
+
+        setMeme({
+            ...meme,
+            randomImage: randomMeme.url
+        })
+    }
+
     return(
         <div>
             <main>
                 <form className="form">
                     <input type="text" placeholder="Top text" className="form--input"/>
                     <input type="text" placeholder="Bottom text" className="form--input"/>
-                    <button className="form--button">Get a new meme image  🖼</button>
+                    <button type="button" className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+                    <img src={meme.randomImage} className="meme--image"></img>
                 </form>
             </main>
         </div>
